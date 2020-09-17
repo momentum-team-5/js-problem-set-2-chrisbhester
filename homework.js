0d9ec793a7ae88bb1f0ac9b3b5af8c03047af852
chrisbhester <chrisnashley2020@gmail.com>
2020-09-17 09:32:40 -0400

    pushing some answers for homework

--- homework.js
+++ homework.js
@@ -2,18 +2,44 @@
 // member of the array, and returns a new array with that member removed.
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
-//
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove(ar, p) {
+    let newarray = []
+    for (let name of ar) {
+        if (name !== p) {
+            newarray.push(name)
+        }
+    }
+    return newarray
+}
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(nums) {
+    let total = 0
+    for (let num of nums) {
+        total += num
+    }
+    return total
+}
+
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(numbers) {
+     let sumOfNums = sum(nums)
+
+     average = sum / arr.length 
+    
+     return average
+     }
+
+   
+
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
